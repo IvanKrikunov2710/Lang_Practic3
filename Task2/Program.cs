1a70882080ac2983b8c29c5f3bee1fa0e6d0f5e2
Ivan <Sernaiv@yandex.ru>
2023-01-26 20:23:42 +0300

    четверть в координаты

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (x и y)
+int kord;
+kord = Convert.ToInt32(Console.ReadLine());
+if (kord == 1)
+Console.WriteLine("x>0, y>0");
+if (kord == 2)
+Console.WriteLine("x<0, y>0");
+if (kord == 3)
+Console.WriteLine("x<0, y<0");
+if (kord == 4)
+Console.WriteLine("x>0, y<0");
