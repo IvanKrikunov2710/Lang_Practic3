d1a9a5d4c3e865f3c913d6ca70228a74099264db
Ivan <Sernaiv@yandex.ru>
2023-01-26 19:45:59 +0300

    нахождение четверти

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] points = new int[2];
+
+for (int i = 0; i < points.Length; i++)
+{
+    points[i] = Convert.ToInt32(Console.ReadLine());
+}
+// points[0] - x, points[1] - y. [[,y]]
+if (points[0] > 0 && points[1] > 0)
+Console.WriteLine("1");
+if (points[0] < 0 && points[1] > 0)
+Console.WriteLine("2");
+if (points[0] < 0 && points[1] < 0)
+Console.WriteLine("3");
+if (points[0] > 0 && points[1] < 0)
+Console.WriteLine("4");
