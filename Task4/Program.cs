3d8863b060e955a03e30763dc70beb6c8f5adfc1
Ivan <Sernaiv@yandex.ru>
2023-01-28 20:56:02 +0300

    расстояние между точками покоординатам

---
+++ Task4/Program.cs
@@ -0,0 +1,7 @@
+﻿double x = 0, y = 0, x1 = 0, y1 = 0, ab = 0;
+x = Convert.ToDouble(Console.ReadLine());
+y = Convert.ToDouble(Console.ReadLine());
+x1 = Convert.ToDouble(Console.ReadLine());
+y1 = Convert.ToDouble(Console.ReadLine());
+ab=Math.Sqrt((x1-x)*(x1-x) + (y1-y)*(y1-y));
+Console.WriteLine(ab);
