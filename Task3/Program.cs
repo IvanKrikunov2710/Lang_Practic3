0fb7a1a2725ddf82b8d9e778bc45b8b647f030c2
Ivan <Sernaiv@yandex.ru>
2023-01-28 20:19:56 +0300

    четверть в координаты

---
+++ Task3/Program.cs
@@ -0,0 +1,8 @@
+﻿string[] output = { "x > 0, y > 0", "x < 0, y > 0", "x < 0, y < 0", "x > 0, y < 0"};
+int number = Convert.ToInt32(Console.ReadLine());
+// 1,2,3,4
+// 0,1,2,3
+if (number < 5 && number > 0)
+    Console.WriteLine(output[number - 1]);
+else
+    Console.WriteLine("input incorrect");
